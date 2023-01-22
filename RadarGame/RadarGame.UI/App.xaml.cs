7da54bca3cd8565be34e6bc2318eec393d46f9d9
AJ <109011451+xAJx1383@users.noreply.github.com>
2023-01-22 14:36:02 +0330

    Add files via upload

--- RadarGame/RadarGame.UI/App.xaml.cs
+++ RadarGame/RadarGame.UI/App.xaml.cs
@@ -58,12 +58,7 @@ namespace RadarGame.UI
             config.AddRule(NLog.LogLevel.Trace, NLog.LogLevel.Fatal, logfile);
             NLog.LogManager.Configuration = config;
 
-            var runningInstances = System.Diagnostics.Process.GetProcessesByName(
-                    System.Diagnostics.Process.GetCurrentProcess().ProcessName);
-            if (runningInstances.Length > 1)
-            {
-                Environment.Exit(0);
-            }
+
 
             var serviceProvider = configureServices();
             this.serviceProvider = serviceProvider;
